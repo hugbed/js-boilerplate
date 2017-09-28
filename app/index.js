b7afa24aa166583f947e5d1cdd6f7d71d763aa3f
Hugo Bedard <Hugo.Bedard@USherbrooke.ca>
2017-09-28 12:56:44 -0400

    Use babel-transform-class-properties

--- app/index.js
+++ app/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Button extends React.Component {
+  // example of babel plugin transform-class-properties
+  handleClick = () => {
+    console.log("Clicked!");
+  };
+
+  render() {
+    return(
+      <button onClick={this.handleClick}>Click me!</button>
+    );
+  }
+}
+
 window.onload = () => {
   ReactDOM.render(
-    <div>World!</div>,
+    <div>
+      <div>World!</div>
+      <Button/>
+    </div>,
     document.getElementById('root')
   );
 }
